e72408306bb59d531aa846ae0f375a14aac2d4f9
zhanghaojie <1455123087@qq.com>
2024-06-08 15:30:46 +0800

    chore: Add todo list and todo list item components
    This commit adds the todo list and todo list item components to the application. The todo list component is responsible for displaying a list of todo items, while the todo list item component represents an individual todo item.
    
    The components are implemented in both the template files (todo-list.component.html and todo-list-item.component.html) and the style files (todo-list.component.less and todo-list-item.component.less).
    
    The todo list component is imported and used in the app component (app.component.ts) to display the list of todo items in the application.
    
    This commit also includes test files (todo-list.component.spec.ts and todo-list-item.component.spec.ts) for both components, which contain basic tests to ensure that the components are created successfully.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LogoComponent } from './logo/logo.component';
+import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LogoComponent],
+  imports: [
+    RouterOutlet,
+    LogoComponent,
+    TodoListComponent,
+    TodoListItemComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+  styleUrl: './app.component.less',
 })
 export class AppComponent {
   title = 'hello-angular';
